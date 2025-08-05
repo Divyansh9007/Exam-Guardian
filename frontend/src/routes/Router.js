bc497fab35899d9f50057c923b4abf38c47d9a39
Divyansh9007 <divyanshpansari123@gmail.com>
2025-08-06 01:29:58 +0530

    fixed router.js

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,43 +1,34 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Navigate, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import Loadable from '../layouts/full/shared/loadable/Loadable';
-import { CircularProgress, Box } from '@mui/material';
-
-// Add a loading fallback component
-const PageLoader = () => (
-  <Box display="flex" justifyContent="center" alignItems="center" minHeight="200px">
-    <CircularProgress />
-  </Box>
-);
 
 /* ***Layouts**** */
-const BlankLayout = Loadable(lazy(() => import('../layouts/blank/BlankLayout')));
-const FullLayout = Loadable(lazy(() => import('../layouts/full/FullLayout')));
-const ExamLayout = Loadable(lazy(() => import('../layouts/full/ExamLayout')));
+import BlankLayout from '../layouts/blank/BlankLayout';
+import FullLayout from '../layouts/full/FullLayout';
+import ExamLayout from '../layouts/full/ExamLayout';
 
 /* ****Pages***** */
-const SamplePage = Loadable(lazy(() => import('../views/sample-page/SamplePage')));
-const Success = Loadable(lazy(() => import('../views/Success')));
+import SamplePage from '../views/sample-page/SamplePage';
+import Success from '../views/Success';
 
 //Student Routes
-const TestPage = Loadable(lazy(() => import('./../views/student/TestPage')));
-const ExamPage = Loadable(lazy(() => import('./../views/student/ExamPage')));
-const ExamDetails = Loadable(lazy(() => import('./../views/student/ExamDetails')));
-const ResultPage = Loadable(lazy(() => import('./../views/student/ResultPage')));
-const StudentResultsList = Loadable(lazy(() => import('./../views/student/StudentResultsList')));
+import TestPage from './../views/student/TestPage';
+import ExamPage from './../views/student/ExamPage';
+import ExamDetails from './../views/student/ExamDetails';
+import ResultPage from './../views/student/ResultPage';
+import StudentResultsList from './../views/student/StudentResultsList';
 
 //Auth Routes
-const Error = Loadable(lazy(() => import('../views/authentication/Error')));
-const Register = Loadable(lazy(() => import('../views/authentication/Register')));
-const Login = Loadable(lazy(() => import('../views/authentication/Login')));
-const UserAccount = Loadable(lazy(() => import('../views/authentication/UserAccount')));
+import Error from '../views/authentication/Error';
+import Register from '../views/authentication/Register';
+import Login from '../views/authentication/Login';
+import UserAccount from '../views/authentication/UserAccount';
 
 // Teacher Routes
-const CreateExamPage = Loadable(lazy(() => import('./../views/teacher/CreateExamPage')));
-const ExamLogPage = Loadable(lazy(() => import('./../views/teacher/ExamLogPage')));
-const AddQuestions = Loadable(lazy(() => import('./../views/teacher/AddQuestions')));
-const PrivateRoute = Loadable(lazy(() => import('src/views/authentication/PrivateRoute')));
-const TeacherRoute = Loadable(lazy(() => import('src/views/authentication/TeacherRoute')));
+import CreateExamPage from './../views/teacher/CreateExamPage';
+import ExamLogPage from './../views/teacher/ExamLogPage';
+import AddQuestions from './../views/teacher/AddQuestions';
+import PrivateRoute from 'src/views/authentication/PrivateRoute';
+import TeacherRoute from 'src/views/authentication/TeacherRoute';
 
 const Router = createBrowserRouter(
   createRoutesFromElements(
@@ -45,164 +36,38 @@ const Router = createBrowserRouter(
       {/* Private Routes */}
       <Route path="" element={<PrivateRoute />}>
         {/* Main layout */}
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <FullLayout />
-            </Suspense>
-          }
-        >
+        <Route path="/" element={<FullLayout />}>
           <Route index element={<Navigate to="/dashboard" />} />
-          <Route
-            path="dashboard"
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <ExamPage />
-              </Suspense>
-            }
-          />
+          <Route path="dashboard" element={<ExamPage />} />
           <Route path="exam" element={<Navigate to="/dashboard" replace />} />
-          <Route
-            path="sample-page"
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <SamplePage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="Success"
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <Success />
-              </Suspense>
-            }
-          />
-          <Route
-            path="exam/:examId/result"
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <ResultPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="student/results/all"
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <StudentResultsList />
-              </Suspense>
-            }
-          />
+          <Route path="sample-page" element={<SamplePage />} />
+          <Route path="Success" element={<Success />} />
+          <Route path="exam/:examId/result" element={<ResultPage />} />
+          <Route path="student/results/all" element={<StudentResultsList />} />
           <Route path="" element={<TeacherRoute />}>
-            <Route
-              path="create-exam"
-              element={
-                <Suspense fallback={<PageLoader />}>
-                  <CreateExamPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="add-questions"
-              element={
-                <Suspense fallback={<PageLoader />}>
-                  <AddQuestions />
-                </Suspense>
-              }
-            />
-            <Route
-              path="exam-log"
-              element={
-                <Suspense fallback={<PageLoader />}>
-                  <ExamLogPage />
-                </Suspense>
-              }
-            />
+            <Route path="create-exam" element={<CreateExamPage />} />
+            <Route path="add-questions" element={<AddQuestions />} />
+            <Route path="exam-log" element={<ExamLogPage />} />
           </Route>
         </Route>
 
         {/* Exam routes using ExamLayout */}
-        <Route
-          path="exam/:examId"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <ExamLayout />
-            </Suspense>
-          }
-        >
-          <Route
-            index
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <ExamDetails />
-              </Suspense>
-            }
-          />
-          <Route
-            path=":testId"
-            element={
-              <Suspense fallback={<PageLoader />}>
-                <TestPage />
-              </Suspense>
-            }
-          />
+        <Route path="exam/:examId" element={<ExamLayout />}>
+          <Route index element={<ExamDetails />} />
+          <Route path=":testId" element={<TestPage />} />
         </Route>
       </Route>
 
       {/* User layout */}
-      <Route
-        path="user"
-        element={
-          <Suspense fallback={<PageLoader />}>
-            <FullLayout />
-          </Suspense>
-        }
-      >
-        <Route
-          path="account"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <UserAccount />
-            </Suspense>
-          }
-        />
+      <Route path="user" element={<FullLayout />}>
+        <Route path="account" element={<UserAccount />} />
       </Route>
 
       {/* Authentication layout */}
-      <Route
-        path="auth"
-        element={
-          <Suspense fallback={<PageLoader />}>
-            <BlankLayout />
-          </Suspense>
-        }
-      >
-        <Route
-          path="404"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <Error />
-            </Suspense>
-          }
-        />
-        <Route
-          path="register"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <Register />
-            </Suspense>
-          }
-        />
-        <Route
-          path="login"
-          element={
-            <Suspense fallback={<PageLoader />}>
-              <Login />
-            </Suspense>
-          }
-        />
+      <Route path="auth" element={<BlankLayout />}>
+        <Route path="404" element={<Error />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
       </Route>
     </>,
   ),
